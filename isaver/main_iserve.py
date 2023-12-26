1132903220df6bf09c5a8a36dadf0b9a15101912
title <wachirapanpimpa2530@gmail.com>
2023-12-26 10:19:41 +0700

    clear_code

--- isaver/main_iserve.py
+++
@@ -1,7 +0,0 @@
-from modbus_iserve import Modbus_Iserve
-mod = Modbus_Iserve()
-class Main_Iserve():
-
-    def start_iserve(self):
-        mod.set_freq2900()
-        
--- restart.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-def restart_programs():
-    print("argv : ", sys.argv)
-    print("sys executable : ", sys.executable)
-    print("Restart Now")
-
-    os.execv(sys.executable,['python'] + sys.argv)

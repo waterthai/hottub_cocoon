881e80073686dc457c7789b19b67a4d1551e1cce
title <wachirapanpimpa2530@gmail.com>
2023-12-26 13:16:58 +0700

    update_time_heater

--- heater/main_heater.py
+++ heater/main_heater.py
@@ -42,17 +42,21 @@ class Main_Heater():
                         print("เปิดปั้ม")
                         read_status_auto = open('/home/pi/hottub_cocoon/txt_file/status_working_heater.txt','w')
                         read_status_auto.write("True")
-                        # read_counter_open = open('/home/pi/hottub_cocoon/txt_file/counter_open_heater.txt','r')
-                        # counter_open_heater = int(read_counter_open.read())
-                        # if counter_open_heater < 60 :
-                        #       sum_counter_heater = counter_open_heater + 1
-                        #       write_counter_open = open('/home/pi/hottub_cocoon/txt_file/counter_open_heater.txt','w')
-                        #       write_counter_open.write(str(sum_counter_heater))
-                        # else:
-                        if plc[2] == False:
-                            mod_heatpump.start_chauffage()
-                        if plc[2] == True:
-                            mod_heatpump.start_chauffage2()
+                        read_counter_open = open('/home/pi/hottub_cocoon/txt_file/counter_open_heater.txt','r')
+                        counter_open_heater = read_counter_open.read().rstrip('\n')
+                        if counter_open_heater < 60 :
+                            try:
+                                sum_counter_heater = int(counter_open_heater) + 1
+                                with open('/home/pi/hottub_cocoon/txt_file/counter_open_heater.txt','w') as write_counter_open:
+                                    write_counter_open.write(str(sum_counter_heater))
+                                    write_counter_open.close()
+                            except:
+                                pass
+                        else:
+                            if plc[2] == False:
+                                mod_heatpump.start_chauffage()
+                            if plc[2] == True:
+                                mod_heatpump.start_chauffage2()
                     elif float(temperature) >= float(data_setting[0]['setting_temperature']): 
                         print("ปิดปั้ม")
                         read_status_auto = open('/home/pi/hottub_cocoon/txt_file/status_working_heater.txt','w')
@@ -133,7 +137,11 @@ class Main_Heater():
                 if plc[1] == True:
                     mod_heatpump.stop_pump_ozone()
     def clear_heater_open_count(self):
-        write_counter_open = open('/home/pi/hottub_cocoon/txt_file/counter_open_heater.txt','w')
-        write_counter_open.write("0")
+        try:
+            with open('/home/pi/hottub_cocoon/txt_file/counter_open_heater.txt','w') as write_counter_open:
+                write_counter_open.write("0")
+                write_counter_open.close()
+        except:
+            pass
 
         
